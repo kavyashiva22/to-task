d7c5d7a62d6193def074a97d7ae6695b531be49f
kavyashiva22 <kavyashiva346@gmail.com>
2025-02-04 19:15:57 +0530

    Add files via upload

---
+++ calculor.py
@@ -0,0 +1,12 @@
+digit1=int(input("enter first number:"))
+op=input("enter your operator:")
+digit2=int(input("enter second number:"))
+
+if op == "+":
+    print(digit1+digit2)
+elif op == "-":
+    print(digit1-digit2)
+elif op == "*":
+    print(digit1*digit2)
+elif op == "/":
+    print(digit1/digit2)
